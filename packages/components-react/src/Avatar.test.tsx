ce20979ab7465efaa63b78df9998bf802a349347
ryanvvalkenhoef <76593083+ryanvvalkenhoef@users.noreply.github.com>
2024-01-02 17:57:17 +0100

    Created tests for react-component

---
+++ packages/components-react/src/Avatar.test.tsx
@@ -0,0 +1,38 @@
+import { describe, expect, it } from '@jest/globals';
+import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { Avatar } from './Avatar';
+import '@testing-library/jest-dom';
+
+describe('Avatar', () => {
+  it('renders a avatar role element', () => {
+    render(<Avatar role="avatar">OK</Avatar>);
+
+    const avatar = screen.getByRole('avatar');
+
+    expect(avatar).toBeInTheDocument();
+    expect(avatar).toBeVisible();
+  });
+
+  it('renders an HTML avatar element', () => {
+    const { container } = render(<Avatar />);
+
+    const avatar = container.querySelector('div:only-child');
+
+    expect(avatar).toBeInTheDocument();
+  });
+
+  it('renders labels that contain HTML rich text content', () => {
+    const { container } = render(
+      <Avatar>
+        <p>now</p>
+      </Avatar>,
+    );
+
+    const avatar = container.querySelector(':only-child');
+
+    const richText = avatar?.querySelector('p');
+
+    expect(richText).toBeInTheDocument();
+  });
+});
