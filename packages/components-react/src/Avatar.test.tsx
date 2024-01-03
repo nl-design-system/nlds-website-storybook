844c06d1eeb47994d854987f763d07615a1cbfdb
ryanvvalkenhoef <76593083+ryanvvalkenhoef@users.noreply.github.com>
2024-01-03 15:41:15 +0100

    Added types to tsconfig.json, excluded test files and removed tsconfig.test.json

--- packages/components-react/src/Avatar.test.tsx
+++ packages/components-react/src/Avatar.test.tsx
@@ -1,8 +1,5 @@
-import { describe, expect, it } from '@jest/globals';
 import { render, screen } from '@testing-library/react';
-import React from 'react';
 import { Avatar } from './Avatar';
-import '@testing-library/jest-dom';
 
 describe('Avatar', () => {
   it('renders a avatar role element', () => {
