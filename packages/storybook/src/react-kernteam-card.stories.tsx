171c8c13c9b52dcbeb934e13eed9104c226d7195
MeesoPost <100311542+MeesoPost@users.noreply.github.com>
2024-01-18 11:04:20 +0100

    🎨 renamed stories

---
+++ packages/storybook/src/react-kernteam-card.stories.tsx
@@ -0,0 +1,48 @@
+import { Meta, StoryObj } from '@storybook/react';
+import { Card } from '../../components-react/src/Card';
+
+const meta = {
+  title: 'React Component/Card',
+  id: 'KernteamCard',
+  component: Card,
+  args: {
+    title: 'Tekst en taalgebruik',
+    description: 'Tekst en taalgebruik in NL Design System',
+    link: {
+      href: '',
+      linktitle: 'Tekst en taalgebruik',
+    },
+  },
+} satisfies Meta<typeof Card>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  title: 'React Component/Card',
+  id: 'KernteamCard',
+  component: Card,
+  args: {
+    title: 'Tekst en taalgebruik',
+    description: 'Tekst en taalgebruik in NL Design System',
+    link: {
+      href: '',
+      linktitle: 'Tekst en taalgebruik',
+    },
+  },
+};
+
+export const Hover: Story = {
+  title: 'React Component/Card',
+  id: 'KernteamCard',
+  component: Card,
+  args: {
+    title: 'Tekst en taalgebruik',
+    description: 'Tekst en taalgebruik in NL Design System',
+    link: {
+      href: '',
+      linktitle: 'Tekst en taalgebruik',
+    },
+  },
+};
