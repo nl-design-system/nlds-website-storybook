afec9982fb0a27b2badbaaeec288c31345407b4d
MeesoPost <100311542+MeesoPost@users.noreply.github.com>
2024-01-18 10:08:04 +0100

    🎨 changed Card to look like the one on the website

--- packages/components-react/src/Card.tsx
+++ packages/components-react/src/Card.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { Heading2, Paragraph } from '@utrecht/component-library-react';
-import { Link } from '@utrecht/component-library-react';
+import { IconArrowNarrowRight } from '@tabler/icons-react';
+import { Heading2, Link, Paragraph } from '@utrecht/component-library-react';
 import clsx from 'clsx';
 import React, { FC, HTMLAttributes } from 'react';
 import './Card.css';
@@ -11,20 +11,22 @@ interface CardProps extends Omit<HTMLAttributes<HTMLDivElement>, 'children'> {
   description: string;
   link: {
     href: string;
+    linktitle: string;
   };
 }
 
-export const Card: FC<CardProps> = ({ description, title, link: { href }, ...props }) => {
+export const Card: FC<CardProps> = ({ description, title, link: { href, linktitle }, ...props }) => {
   const linkRef = React.useRef<HTMLAnchorElement>(null);
 
   return (
-    <div {...props} className={clsx('utrecht-card', props.className)} onClick={() => linkRef.current?.click()}>
-      <div className={'utrecht-card__content'}>
-        <Heading2 className="utrecht-card__title">
-          {title}
-          <Paragraph className="utrecht-card__body">{description}</Paragraph>
-          <Link ref={linkRef} href={href} className={'utrecht-link utrecht-link--html-a'}></Link>
-        </Heading2>
+    <div {...props} className={clsx('kernteam-card', props.className)} onClick={() => linkRef.current?.click()}>
+      <div className={'kernteam-card__content'}>
+        <Heading2 className="kernteam-card__title">{title}</Heading2>
+        <Paragraph className="kernteam-card__body">{description}</Paragraph>
+        <Link ref={linkRef} href={href} className={'kernteam-link kernteam-link--html-a'}>
+          {linktitle}
+          <IconArrowNarrowRight />
+        </Link>
       </div>
     </div>
   );
