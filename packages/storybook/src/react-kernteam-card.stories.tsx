457a5a0e9a688b02de2dc38dffbf0367eb74b6a2
MeesoPost <100311542+MeesoPost@users.noreply.github.com>
2024-01-18 16:42:07 +0100

    ✨ Trying to add hover pseudo parameter

--- packages/storybook/src/react-kernteam-card.stories.tsx
+++ packages/storybook/src/react-kernteam-card.stories.tsx
@@ -4,12 +4,13 @@ import { Card } from '../../components-react/src/Card';
 const meta = {
   title: 'React Component/Card',
   id: 'KernteamCard',
+  tags: ['autodocs'],
   component: Card,
   args: {
     title: 'Tekst en taalgebruik',
     description: 'Tekst en taalgebruik in NL Design System',
     link: {
-      href: '',
+      href: '#',
       linktitle: 'Tekst en taalgebruik',
     },
   },
@@ -24,19 +25,18 @@ export const Default: Story = {
     title: 'Tekst en taalgebruik',
     description: 'Tekst en taalgebruik in NL Design System',
     link: {
-      href: '',
+      href: '#',
       linktitle: 'Tekst en taalgebruik',
     },
   },
 };
 
+/**Card in hover state*/
 export const Hover: Story = {
+  parameters: {
+    pseudo: { hover: true },
+  },
   args: {
-    title: 'Tekst en taalgebruik',
-    description: 'Tekst en taalgebruik in NL Design System',
-    link: {
-      href: '',
-      linktitle: 'Tekst en taalgebruik',
-    },
+    ...Default.args,
   },
 };
