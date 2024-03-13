138dec365bbfe7d4f1305c3d8b2d81adcfc90beb
matijs <matijs@gmail.com>
2024-03-13 11:49:02 +0100

    build: install major updates
    Add `storybook` and `@storybook/*` to .ncurc.major.js to stay on
    Storybook v7 for now.

--- .ncurc.major.js
+++ .ncurc.major.js
@@ -2,6 +2,6 @@ const minorConfig = require('./.ncurc.minor');
 
 module.exports = {
   ...minorConfig,
-  reject: ['@mdx-js/react', ...minorConfig.reject],
+  reject: [...minorConfig.reject, '@mdx-js/react', 'storybook', '@storybook/*'],
   target: 'latest',
 };
