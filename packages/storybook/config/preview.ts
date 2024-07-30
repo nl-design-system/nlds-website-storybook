b52efe3bfa7a52b843059475ac1b8ac5d7920f1d
Bryan de Jong <bryan@levitade.io>
2024-07-30 11:51:25 +0200

    chore: remove web components

--- packages/storybook/config/preview.ts
+++ packages/storybook/config/preview.ts
@@ -1,10 +1,7 @@
 // import '@nl-design-system-unstable/voorbeeld-design-tokens/src/font.js';
-import { defineCustomElements } from '@kernteam/web-components-stencil/loader/index.js';
 import '@nl-design-system-unstable/voorbeeld-design-tokens/dist/index.css';
 import type { Preview } from '@storybook/react';
 import { ParameterArgsDecorator } from './ParameterArgsDecorator';
-
-defineCustomElements();
 
 const preview: Preview = {
   decorators: [ParameterArgsDecorator],
